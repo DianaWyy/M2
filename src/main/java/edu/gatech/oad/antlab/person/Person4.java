e08ae624943ca3e2294e7db562c4ab23e8076e14
Frankxhy <54780802+Frankxhy@users.noreply.github.com>
2019-09-15 23:17:58 -0400

    Update Person4.java

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,8 +30,11 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      String ans = ""
+      for(int i = 0; i < input.length(); i++){
+        ans += (char) (input.charAt(i) + 1);
+      }
+      return ans;
     }
     
     /**
