54237903685872a81cbd4508c39b044144b2428b
Eric Gu <egu8@gatech.edu>
2019-09-16 11:32:30 -0400

    Fix Minor Issues

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -37,7 +37,7 @@ public class Person1 {
     private String calc(String input) {
         //Person 1 put your implementation here
         int length = input.length();
-        if (length < 2) {
+        if (length <= 2) {
             return input;
         }
 
