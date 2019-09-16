c0d97b1f3bdf8242b566e54b7787cffe3b429480
TigerSHe1998 <55221117+TigerSHe1998@users.noreply.github.com>
2019-09-16 15:36:21 -0400

    Update AntLabMain.java

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -66,7 +66,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Haorong She");
-		  toPrint += p3.toString("hshe3");
+		  toPrint += p3.toString("903437267");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("Haoyun Xu");
